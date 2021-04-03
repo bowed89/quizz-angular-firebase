24b118e3e69ce1a1c6b6dde8bef604bbe15b8a80
Jesús López Averanga <jesus.lopez.averanga@gmail.com>
2021-04-02 20:03:45 -0400

    Juego de preguntas desarrollado en angular y firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAOO2L77RBsV3RehzAId_-BF-dQAufc03A",
+    authDomain: "quizz-55490.firebaseapp.com",
+    projectId: "quizz-55490",
+    storageBucket: "quizz-55490.appspot.com",
+    messagingSenderId: "337982216156",
+    appId: "1:337982216156:web:74af082e78d0b11b622119"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOO2L77RBsV3RehzAId_-BF-dQAufc03A",
+    authDomain: "quizz-55490.firebaseapp.com",
+    projectId: "quizz-55490",
+    storageBucket: "quizz-55490.appspot.com",
+    messagingSenderId: "337982216156",
+    appId: "1:337982216156:web:74af082e78d0b11b622119"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
